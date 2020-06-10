3425cc85b6867d0b55b0642890ac60dbcf53a695
Abbey Brown <abbeybrownsemail@gmail.com>
2020-06-09 22:30:49 -0500

    objects challenge complete

---
+++ main.js
@@ -0,0 +1,38 @@
+let elizabethSanger = {
+ congressionalDistrict : "Tennessee",
+ platformIssues : [ {
+  Taxes : "Moderate. Low taxes for lower/middle class",
+  Jobs : "Supports non-discrimination jobs and works to provide them",
+  Infrastructure : "Promotes infrastructure for living well-being and jobs",
+  HealthCare : "healthcare for all",
+  CrimeAndEnforcement: "law enforcement will monitored",
+ }
+],
+URLDonationForm : "donate@enlizabethsanger.com",
+CalendarOfEvents : "calendar@enlizabethsanger.com",
+VolunteerInformation : {
+Name : "Person Name",
+Address : "123 Sesame Street",
+Email: "hello@gmail.com",
+PhoneNumber: "123-4567",
+Availability: "Full Availability",
+ActivityAvailability: "answering phone calls, taking polls", 
+},
+Biography: "Started from the bottom",
+ImageGallery: [
+  {
+    img1: "img1.jpg",
+    img2: "img2.jpg",
+    img3: "img3.jpg",
+  }
+],
+HeadShot: "imgCongresswoman.jpg", 
+PictureOFFamily: "imgCongresswomansFamily.jpg",
+PictureOfConstituents: "imgConstituents.jpg",
+MissionStatement: "Congresswoman for the people, not the politicians.",
+URLRegisterToVote: "voteTennessee.org"
+
+}
+
+console.log(elizabethSanger);
+console.log("Hello")
